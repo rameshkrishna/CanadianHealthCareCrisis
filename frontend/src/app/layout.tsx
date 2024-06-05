f0ae9ccfebba7a981ea1b82b192a022e53837cd5
Ramesh Krishna <rameshkrishna459@gmail.com>
2024-06-05 13:07:43 +0530

    remove testing framework and fix search

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,15 +28,6 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      {(process.env.NODE_ENV === "development" ||
-        process.env.VERCEL_ENV === "preview") && (
-        // eslint-disable-next-line @next/next/no-sync-scripts
-        <script
-          data-project-id="1lxftfbJBRdsvBmnkYAkFmAMqucT5mcpf53Ij9Hh"
-          data-is-production-environment="false"
-          src="https://snippet.meticulous.ai/v1/meticulous.js"
-        />
-      )}
       <head />
       {/* <GoogleAnalytics gaId="G-SFDW8W0TZ0" /> */}
       {/* <GoogleTagManager gtmId="G-SFDW8W0TZ0" /> */}
